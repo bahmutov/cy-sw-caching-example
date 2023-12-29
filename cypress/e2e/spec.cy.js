c90570e6eb09c206e9dfdf93237114aee4f774ac
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-29 17:33:14 -0500

    the initial code

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,44 +5,14 @@
 // https://github.com/bahmutov/cypress-cdp
 import 'cypress-cdp'
 
-// simply bypass the service worker
-// when requesting the network resources
-// including the next spec run
-beforeEach(() => {
-  cy.CDP('Network.setBypassServiceWorker', {
-    bypass: true,
-  })
-})
-
 it('loads the updated test script', () => {
   cy.visit('/')
+  // change the number and confirm the
+  // updated test runs right away
   cy.log('1')
 })
 
+// after the tests are done
 // delete the "build" cache that can cache the spec file
-// after(() => {
-//   const baseUrl = Cypress.config('baseUrl')
-//   cy.CDP('CacheStorage.requestCacheNames', {
-//     securityOrigin: baseUrl,
-//   })
-//     .its('caches')
-//     .invoke('find', (cache) => cache.cacheName === 'build')
-//     .then((buildCache) => {
-//       if (buildCache) {
-//         cy.CDP('CacheStorage.deleteCache', {
-//           cacheId: buildCache.cacheId,
-//         })
-//       }
-//     })
-// })
-
-// delete the service worker to stop using
-// the cached version on the next test run
-// after(() => {
-//   const scopeURL = Cypress.config('baseUrl')
-//   cy.CDP('ServiceWorker.enable')
-//   cy.CDP('ServiceWorker.stopAllWorkers')
-//   cy.CDP('ServiceWorker.unregister', {
-//     scopeURL,
-//   })
-// })
+// https://chromedevtools.github.io/devtools-protocol/
+// use the "CacheStorage" interface
