d23dbed16a82eb8eed97b528717934310881f9a7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-30 09:32:09 -0500

    working solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,22 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 
+// https://github.com/bahmutov/cypress-cdp
+import 'cypress-cdp'
+
 it('loads the updated test script', () => {
   cy.visit('/')
   cy.log('1')
+})
+
+// delete the service worker to stop using
+// the cached version on the next test run
+// Tip: you must enable ServiceWorker CDP module
+after(() => {
+  const scopeURL = Cypress.config('baseUrl')
+  cy.CDP('ServiceWorker.enable')
+  cy.CDP('ServiceWorker.stopAllWorkers')
+  cy.CDP('ServiceWorker.unregister', {
+    scopeURL,
+  })
 })
